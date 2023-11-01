6efb611584b0f3de25634c9480c8c1cf4b4a869c
marcelfinsera <marcel@finsera.com>
2023-10-31 23:02:21 -0300

    Fixed fivacon

--- age-calculator/app/layout.tsx
+++ age-calculator/app/layout.tsx
@@ -1,25 +1,22 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700', '800'] })
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700", "800"] });
 
 export const metadata: Metadata = {
-  title: 'Frontend Mentor | Age calculator app',
-  description: 'Frontend Mentor | Age calculator app',
-  icons: {
-    icon: '/favicon.png'
-  }
-}
+  title: "Frontend Mentor | Age calculator app",
+  description: "Frontend Mentor | Age calculator app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
-  )
+  );
 }
