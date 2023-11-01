ee409c70f68b41fe86c8923b57cb6843c5bd7d60
marcelfinsera <marcel@finsera.com>
2023-10-31 22:58:12 -0300

    Age Calculator Challenge

---
+++ age-calculator/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700', '800'] })
+
+export const metadata: Metadata = {
+  title: 'Frontend Mentor | Age calculator app',
+  description: 'Frontend Mentor | Age calculator app',
+  icons: {
+    icon: '/favicon.png'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
